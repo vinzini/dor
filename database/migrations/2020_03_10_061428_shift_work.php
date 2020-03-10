c0f38772f4811401655c6046fce6bec867af1810
vinay <vinay.p@ziniostech.com>
2020-03-10 16:46:17 +0530

    migrations completed

--- database/migrations/2020_03_10_061428_shift_work.php
+++ database/migrations/2020_03_10_061428_shift_work.php
@@ -15,9 +15,85 @@ class ShiftWork extends Migration
     {
         Schema::create('shift_work', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('staff_id');
+            $table->integer('main_group_id');
+            $table->integer('management_group_id');
+            $table->integer('shift_plan_id');
+            $table->integer('company_id');
             $table->timestamps();
+            $table->integer('created_id');
+            $table->integer('updated_id');
+            $table->date('work_date');
+            $table->integer('work_month');
+            $table->string('attendance_code');
+            $table->timestamp('start_plan');
+            $table->timestamp('end_plan');
+            $table->timestamp('start_time_clock');
+            $table->timestamp('end_time_clock');
+            $table->timestamp('start_work');
+            $table->timestamp('end_work');
+            $table->timestamp('start_work_round');
+            $table->timestamp('end_work_round');
+            $table->timestamp('start_early_extra');
+            $table->timestamp('end_early_extra');
+            $table->timestamp('start_early_extra_round');
+            $table->timestamp('end_early_extra_round');
+            $table->timestamp('start_extra');
+            $table->timestamp('end_extra');
+            $table->timestamp('start_extra_round');
+            $table->timestamp('end_extra_round');
+            $table->timestamp('start_all');
+            $table->timestamp('end_all');
+            $table->timestamp('start_all_round');
+            $table->timestamp('end_all_round');
+            $table->smallInteger('time_round');
+            $table->string('start_time_clock_source');
+            $table->string('end_time_clock_source');
+            $table->jsonb('rest_times');
+            $table->smallInteger('edit_rest');
+            $table->integer('work_time');
+            $table->integer('rest_time');
+            $table->integer('late_time');
+            $table->integer('early_time');
+            $table->integer('late_edit');
+            $table->integer('early_edit');
+            $table->integer('late_cancel');
+            $table->integer('early_cancel');
+            $table->smallInteger('is_edit');
+            $table->smallInteger('personal_approval');
+            $table->timestamp('personal_approval_date');
+            $table->smallInteger('group_approval');
+            $table->integer('group_approval_id');
+            $table->timestamp('group_approval_date');
+            $table->smallInteger('main_group_approval');
+            $table->integer('main_group_approval_id');
+            $table->timestamp('main_group_approval_date');
+            $table->smallInteger('company_approval');
+            $table->integer('company_approval_id');
+            $table->timestamp('company_approval_date');
+            $table->jsonb('others_approval_list');
+            $table->integer('calendar_date_type');
+            $table->integer('work_date_type');
+            $table->string('timezone');
+            $table->integer('utc_offset');
+            $table->timestamp('order_key');
+            $table->smallInteger('unsolved_plan');
+            $table->timestamp('auto_time_clock_start');
+            $table->timestamp('auto_time_clock_end');
+            $table->string('note');
+            $table->integer('attendance_status');
+            $table->timestamp('extra_updated_at');
+            $table->integer('before_notify_status');
+            $table->timestamp('before_notify_time');
+            $table->integer('late_notify_status');
+            $table->timestamp('late_notify_time');
+            $table->integer('early_notify_status');
+            $table->integer('extra_notify_status');
+            $table->timestamp('extra_notify_time');
+            $table->timestamp('work_notify_time');
         });
     }
+
 
     /**
      * Reverse the migrations.
--- database/migrations/2020_03_10_061439_shift_work_detail.php
+++ database/migrations/2020_03_10_061439_shift_work_detail.php
@@ -15,9 +15,51 @@ class ShiftWorkDetail extends Migration
     {
         Schema::create('shift_work_detail', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('shift_work_id');
+            $table->integer('staff_id');
+            $table->integer('company_id');
             $table->timestamps();
+            $table->integer('created_id');
+            $table->integer('updated_id');
+            $table->integer('labor_condition_id');
+            $table->integer('fixed_working_time');
+            $table->integer('overtime');
+            $table->integer('night_time');
+            $table->integer('night_overtime');
+            $table->integer('legal_overtime');
+            $table->integer('non_legal_overtime');
+            $table->integer('legal_night_overtime');
+            $table->integer('non_legal_night_overtime');
+            $table->integer('early_extra_time');
+            $table->integer('extra_time');
+            $table->jsonb('time_list');
+            $table->jsonb('total_time_list');
+            $table->integer('pay_type');
+            $table->decimal('commutation_allowance',19,4);
+            $table->smallInteger('transportation_auto');
+            $table->smallInteger('transportation_edit');
+            $table->decimal('transportation_expenses',19,4);
+            $table->decimal('fixed_labor_cost',19,4);
+            $table->decimal('overtime_labor_cost',19,4);
+            $table->decimal('labor_cost',19,4);
+            $table->jsonb('labor_cost_list',19,4);
+            $table->jsonb('allowances_deductions',19,4);
+            $table->double('start_time_lat',20,4);
+            $table->double('start_time_lon',20,4);
+            $table->double('end_time_lat',20,4);
+            $table->double('end_time_lon',20,4);
+            $table->integer('fixed_working_hours_day');
+            $table->decimal('base_wage',19,4);
+            $table->decimal('allowance',19,4);
+            $table->decimal('deduction',19,4);
+            $table->smallInteger('variable_week_no');
+            $table->decimal('paid_leave_allowance',19,4);
+            $table->smallInteger('daily_wage_given');
+            $table->decimal('overtime_base_wage',19,4);
         });
     }
+
+
 
     /**
      * Reverse the migrations.
